8a887b53f321a2076c376261a4f5ade02955781f
zeeshanasim78 <zeeshanasim78@yahoo.com>
2022-10-09 18:34:23 +0500

    Re arrangement of Folders
    Re arrangement of Folders

---
+++ JavaScript/Lecture-1.js
@@ -0,0 +1,10 @@
+// This is Lecture # 1 of the Java Script
+
+// How to Add Comment in Java Script
+// In Java Script To add a comment in VS Code in Windows we can use the Shortcut Key Ctrl+K+C
+// You can also comment out a line by simply writting two // at the start of the line
+
+// How to show a message in MessageBox in Java Script
+// To show a text in the message window in the Java we use alert() Function
+// We user the alert function we need to pass a string containing a message to display
+alert("Welcome to Java Script")
