85a2e22852b0534168254c519198aef932c0a2fd
zeeshanasim78 <zeeshanasim78@yahoo.com>
2023-02-19 00:26:26 +0500

    New Books Added

---
+++ Web-3.0/Quarter-1/TypeScript/Assignments/3/index.js
@@ -0,0 +1,6 @@
+"use strict";
+/* Assignment # 3
+Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+*/
+let pName = 'Zeeshan Asim';
+console.log(` Name in Upper Case ${pName.toUpperCase}`);
