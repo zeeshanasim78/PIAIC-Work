d1305d004931addb3bacfea6cdf683c8a44ebb90
Zeeshan <webdevsols@gmail.com>
2023-03-09 17:55:18 +0500

    Array Functions
    Working With Arrays and functions

--- Web-3.0/Quarter-1/TypeScript/Lecture-3/if-else.js
+++ Web-3.0/Quarter-1/TypeScript/Lecture-3/if-else.js
@@ -1,20 +1,18 @@
+"use strict";
 // If , Else and Else if
-
-let marks:number=78;
-
-if(marks >= 90){
+let marks = 78;
+if (marks >= 90) {
     console.log("A+ Grade");
 }
-else if(marks >= 80){
+else if (marks >= 80) {
     console.log("A Grade");
 }
-else if(marks >= 70){
+else if (marks >= 70) {
     console.log("B Grade");
 }
-else if(marks >= 60){
+else if (marks >= 60) {
     console.log("C Grade");
 }
-else{
+else {
     console.log(" Fail ");
-    
 }
