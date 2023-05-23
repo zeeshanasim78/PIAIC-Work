48ff04ae9228362f5c65cc25f5e8904ab7c53976
Zeeshan <webdevsols@gmail.com>
2023-05-23 08:44:17 +0500

    project using shadcn
    Building a Next.js project using shadcn

--- Web-3.0/Quarter-1/Next.js/Practice/step01-routes/src/app/page.tsx
+++ Web-3.0/Quarter-1/Next.js/Practice/step01-routes/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <b><u>Routes in Next.js </u></b>    <br></br>
+          You are at h
           You Can Visit following Routes : 
           <a href=".\info\pages.tsx"> About Us </a>
           <a href=".\news\pages.tsx"> News </a>
