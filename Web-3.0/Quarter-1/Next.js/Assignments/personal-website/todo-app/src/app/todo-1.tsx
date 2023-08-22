3f912b122f5416f5fd57a0b3a53de7660c758e47
Zeeshan <webdevsols@gmail.com>
2023-08-22 09:57:25 +0500

    Removed

--- Web-3.0/Quarter-1/Next.js/Assignments/personal-website/todo-app/src/app/todo-1.tsx
+++
@@ -1,9 +0,0 @@
-export default function AddTodo1()
-{
-   return (
-    <div className=" bg-blue-300" >
-        <input type="text"></input>
-        <button className='bg-blue-200 border-spacing-3 px-2'> Add Button </button>
-    </div>
-   ) 
-}
